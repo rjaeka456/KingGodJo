be9482eeeea84efd3917835b051854d30842f1ee
rjaeka456 <hoon4569@gmail.com>
2017-03-29 18:52:57 +0900

    first commit

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define TEST 5
+
+int main(void)
+{
+	const int x = 3;
+	int y = (__builtin_constant_p (x) ? (x) : -1);
+	int z = (__builtin_constant_p (TEST) ? (TEST) : -1);
+	char buf[256];
+
+	sprintf(buf, "test : %d\n", y);
+	puts(buf);
+	sprintf(buf, "test2 : %d\n", z);
+	puts(buf);
+	return 0;
+}
